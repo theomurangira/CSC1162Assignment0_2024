d9b764f79bec6075899e885e525411535fd65a0b
Theo Murangira <theoneste@yahoo.com>
2024-10-13 15:31:08 +0200

    Changed the file

---
+++ Savings.py
@@ -0,0 +1,13 @@
+annual_salary=int(input("How much do you take home annually?"))
+monthly_salary=annual_salary/12
+saving_rate=float(input("What percentage d you save monthly?"))
+annual_interest_rate=float(input("What is the annual interest on your savings?"))
+house_price=int(input("What is the price of your dream house?"))
+down_payment=house_price*0.25
+monthly_saving =monthly_salary*saving_rate
+savings =0
+number_month=0
+while savings < down_payment:
+    #your code goes here
+    pass
+print(f"You have reached {savings} after {number_month} months")
